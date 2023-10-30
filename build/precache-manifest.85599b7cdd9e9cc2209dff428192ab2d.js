738161b08a0fc10792bc372344dd2e10b2178b88
Sai Keerthana Arun <64070455+Kits1700@users.noreply.github.com>
2023-10-30 13:24:27 +0530

    Add files via upload

---
+++ build/precache-manifest.85599b7cdd9e9cc2209dff428192ab2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "864e4f70a532497bda3f",
+    "url": "/static/css/main.38e1b5cc.chunk.css"
+  },
+  {
+    "revision": "864e4f70a532497bda3f",
+    "url": "/static/js/main.864e4f70.chunk.js"
+  },
+  {
+    "revision": "4e0526303476771c099b",
+    "url": "/static/js/1.4e052630.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "633fd9592148c9ae0dc9c9cbb22df905",
+    "url": "/static/media/bg14.633fd959.jpg"
+  },
+  {
+    "revision": "3de425f1fbf782a8df322e200f8451ab",
+    "url": "/index.html"
+  }
+];
